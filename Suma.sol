ba2b110f71100a9b2baeee9eccab3e862095caa8
Ariel Ayala <arielayalamail@gmail.com>
2021-11-08 07:58:30 -0300

    Initial commit

---
+++ Suma.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Indentifier: MIT
+pragma solidity >=0.7.0 < 0.9.0; // version del compilador
+
+contract Suma {
+    function sumar(int numero1, int numero2) public pure returns (int) {
+        return numero1 + numero2;
+    }
+}
